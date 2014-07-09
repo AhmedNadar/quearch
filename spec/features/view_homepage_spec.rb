be5d53ed4af8af8bbdd17e4ed9e38d509186d512
Ahmed Nadar <ahmedfay@hotmail.com>
2014-07-08 23:49:45 -0400

    Pass view home page spec

--- spec/features/view_homepage_spec.rb
+++ spec/features/view_homepage_spec.rb
@@ -3,6 +3,8 @@ feature "View the home page" do
   scenario "users should sees relevant information" do
     visit root_path
     expect(page).to have_css 'title', text: "Lit Temple", visible: false 
-    expect(page).to have_css 'h5', text: 'sec-heading'
+    expect(page).to have_content 'Reddit:'
+		expect(page).to have_text 'Looking for something simple?'
+    expect(page).to have_css '.sec-heading'
   end
 end
