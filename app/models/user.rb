ed04975fbad6c60dcbcb9b77c7d4d1a88ceb5e89
Ahmed Nadar <ahmedfay@hotmail.com>
2014-07-13 03:00:34 -0400

    Columns to Users table and update Users controller as well as views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :confirmable,:recoverable, :lockable, :timeoutable, :trackable, and :omniauthable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
+
+  validates :username, :first_name, :last_name, presence: true
+  validates :username, length: { minimum: 2 }
+  validates :username, length: { maximum: 7 }
+  validates :password, length: { in: 6..20 }
+  validates :email, :user, uniqueness: true
+
+	def full_name
+		first_name.capitalize + " " + last_name.capitalize        	
+	end         
 end
