42434a1774dcf77c10f7e59f8f87f3d84fa08738
Ahmed Nadar <ahmedfay@hotmail.com>
2014-07-12 23:53:26 -0400

    update front end work

--- app/models/twitter_client.rb
+++ app/models/twitter_client.rb
@@ -5,10 +5,10 @@ class TwitterClient
 
   def self.client
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = Rails.application.secrets.twitter_api_token
-      config.consumer_secret     = Rails.application.secrets.twitter_api_secret
-      config.access_token        = Rails.application.secrets.twitter_access_token
-      config.access_token_secret = Rails.application.secrets.twitter_access_token_secret
+      config.consumer_key        = 'jso686ZJoBZ93ddY7RwZfcQRd'
+      config.consumer_secret     = 'UdCe4MI1dxSzi1HjSzHLVwgV5qzGW80T7R429ua8muxCAQjH29'
+      config.access_token        = '18496288-6KIkqjrDkuJnaSZIMS8Rjw0OrncFt4JxGZIlul6YT'
+      config.access_token_secret = 'ziWIkcoecpEMJVk2MTJxJSXj5yPFdipcZlXCWPYL3jxEP'
     end
   end
 end
